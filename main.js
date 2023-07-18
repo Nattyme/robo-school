2d37a5a5ea6f918b53c4a63002660f27d762a039
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2023-07-18 15:21:01 +0300

    JS btnPrev added

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const slider = document.querySelector('#slider');
 const sliderItems = Array.from(slider.children);
 const btnNext = document.querySelector('#btnNext');
+const btnPrev = document.querySelector('#btnPrev');
 
 
 sliderItems.forEach(function(slide, index) {
@@ -49,6 +50,26 @@ btnNext.onclick = function(){
 	
 	//Показываем след слайд
 	const nextSlideIndex = currentSlideIndex + 1 === sliderItems.length ? 0 : currentSlideIndex + 1;
-
-	
+	const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
+	nextSlide.classList.remove('hidden');
+	nextSlide.setAttribute('data-active', '');
 }
+
+btnPrev.onclick = function(){
+	console.log('Prev Slide');
+	
+	//Скрываем текущий слайд
+	const currentSlide = slider.querySelector('[data-active]');
+	const currentSlideIndex = +currentSlide.dataset.index;
+	
+	currentSlide.classList.add('hidden');
+	currentSlide.removeAttribute('data-active');
+	
+	//Показываем след слайд
+	const nextSlideIndex = currentSlideIndex === 0 ? sliderItems.length - 1 : currentSlideIndex - 1;
+	
+	const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
+	nextSlide.classList.remove('hidden');
+	nextSlide.setAttribute('data-active', '');
+}
+
