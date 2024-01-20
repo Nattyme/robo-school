853f8e09886c016b8e6bb97c7c74226530baf00c
NeuralNat <ifrance@yandex.ru>
2024-01-21 00:30:26 +0300

    HTML/ CSS/ JS slider base data

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 // Находим нужные элементы на странице - кнопка модалки, модалка, кнопка закрытия
-const button = document.querySelector('[data-modal-button]');
-const modal = document.querySelector('[data-modal]');
-const buttonClose = document.querySelector('[data-modal-close]');
+// const button = document.querySelector('[data-modal-button]');
+// const modal = document.querySelector('[data-modal]');
+// const buttonClose = document.querySelector('[data-modal-close]');
 const offers = document.querySelectorAll('.offer');
 
 
-// Прослушиваем клик на кнопке Открытия, открываем модалку
-button.addEventListener('click', function(){ 
-    modal.classList.remove('none');
-});
+// // Прослушиваем клик на кнопке Открытия, открываем модалку
+// button.addEventListener('click', function(){ 
+//     modal.classList.remove('none');
+// });
 
-// Прослушиваем клик по кнопке закрытия, закрываем модалку
-buttonClose.addEventListener('click', function(){
-    modal.classList.add('none');
-    console.log('click');
-})
+// // Прослушиваем клик по кнопке закрытия, закрываем модалку
+// buttonClose.addEventListener('click', function(){
+//     modal.classList.add('none');
+//     console.log('click');
+// });
 
-modal.addEventListener('click', function(){
+// modal.addEventListener('click', function(){
    
     
-});
+// });
 
 // offer section card color change
 for (let offer of offers) {
@@ -30,10 +30,15 @@ for (let offer of offers) {
         offer.classList.remove('offer--active');
     }
 }
-
-
 // const mouseoutHandler = function(){
 //     offer.classList.remove('offer--active');
 // }
 // offer.onmouseout = mouseoutHandler;
-console.log(offers)
+
+// Вызов карусели
+$(document).ready(function(){
+    $("#slider-teachers").owlCarousel({
+        items: 4,
+        loop: true
+    });
+  });
