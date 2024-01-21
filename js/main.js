623901a09167864b3ea3991c7fca2d24e9b691fc
NeuralNat <ifrance@yandex.ru>
2024-01-21 13:40:05 +0300

    SCSS .cta section bg correction

--- js/main.js
+++ js/main.js
@@ -1,25 +1,4 @@
-// Находим нужные элементы на странице - кнопка модалки, модалка, кнопка закрытия
-// const button = document.querySelector('[data-modal-button]');
-// const modal = document.querySelector('[data-modal]');
-// const buttonClose = document.querySelector('[data-modal-close]');
 const offers = document.querySelectorAll('.offer');
-
-
-// // Прослушиваем клик на кнопке Открытия, открываем модалку
-// button.addEventListener('click', function(){ 
-//     modal.classList.remove('none');
-// });
-
-// // Прослушиваем клик по кнопке закрытия, закрываем модалку
-// buttonClose.addEventListener('click', function(){
-//     modal.classList.add('none');
-//     console.log('click');
-// });
-
-// modal.addEventListener('click', function(){
-   
-    
-// });
 
 // offer section card color change
 for (let offer of offers) {
@@ -38,7 +17,7 @@ for (let offer of offers) {
 // Вызов карусели
 $(document).ready(function(){
     $("#slider-teachers").owlCarousel({
-        items: 4,
+        items: 3,
         loop: true
     });
   });
